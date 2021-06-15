793b3c20ecee311a7e7098bb5882a7679684632c
Nguyễn Quân <quanhien1999@gmail.com>
2021-06-15 21:50:17 +0700

    draft

--- src/services/config.js
+++ src/services/config.js
@@ -1,7 +1,7 @@
 let config = {
   // config host for be
   // host: "http://localhost:44310",
-  host: "http://03862f9f1ab3.ngrok.io",
+  host: "http://fed81d9e1116.ngrok.io",
   debug: true,
 };
 export default config;
