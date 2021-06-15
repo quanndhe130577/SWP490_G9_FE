f06cdd0ca01d1a1c3763b359360069df2c59ce97
Nguyễn Quân <quanhien1999@gmail.com>
2021-06-15 23:51:33 +0700

    Revert "turn off check otp"
    This reverts commit f656ea81c4d08f102b57b2a826434a5f4762d2ad.

--- src/views/Entry/Register/Step1.js
+++ src/views/Entry/Register/Step1.js
@@ -16,7 +16,7 @@ export default class Step1 extends Component {
           <Widgets.Text
             required={true}
             // label={i18n.t("OTP")}
-            value={value}
+            value={value || "123456"}
             onChange={onChange}
             // submitted={submitted}
           />
