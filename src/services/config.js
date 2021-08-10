658556185199a17a0db7758d714e6aeb5d167dbc
Taam <dvtam99@gmail.com>
2021-08-11 02:18:26 +0700

    change port

--- src/services/config.js
+++ src/services/config.js
@@ -1,9 +1,6 @@
 let config = {
   // config host for be
-  host: "https://c3fe615f6d6e.ngrok.io",
-  // host: "https://localhost:44310",
-
-  //host: "http://c3fe615f6d6e.ngrok.io",
+  host: "http://localhost:5000",
   // debug: true,
 };
 export default config;
