b20b202d13fa8e935a396f9191490c86f8a96c21
Taam <dvtam99@gmail.com>
2021-08-28 19:50:25 +0700

    .

--- src/services/config.js
+++ src/services/config.js
@@ -1,7 +1,7 @@
 let config = {
   // config host for be
-  //host: "http://localhost:5000",
-   host: "https://localhost:44310",
+  host: "http://localhost:5000",
+  //  host: "https://localhost:44310",
   // debug: true,
 };
 export default config;
